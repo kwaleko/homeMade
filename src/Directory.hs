ac29eaac9af70a5348d4e57de2c5731e0d8c0ef7
kwaleko <khaledalomar@tutanota.com>
2020-02-15 12:30:02 +0300

    migrate css func

--- src/Directory.hs
+++ src/Directory.hs
@@ -11,6 +11,8 @@ import           System.Directory               ( getDirectoryContents
                                                 , removeFile
                                                 , removeDirectoryRecursive
                                                 , doesDirectoryExist
+                                                , copyFile
+                                               
                                                 )
 import           Control.Monad                  ( forM
                                                 , forM_
@@ -19,11 +21,13 @@ import           Control.Monad                  ( forM
 import           Control.Monad.IO.Class         ( liftIO )
 import           System.FilePath                ( (</>) )
 import           Control.Concurrent.Async       ( concurrently_ )
+import qualified Data.Map                      as Map
+import           Data.Text                      ( pack )
 
 data Config = Config {sourceDir :: FilePath
                      ,destDir   :: FilePath
                      ,templates :: String -- name of the folder holding all templates
-                     ,markdown  :: String -- name of the folder hodling the markdown posts
+                     ,markdown  :: String -- name of the folder hodling the markdown posts 
                      }
 
 readArchive :: FilePath -> IO [Post.Post]
@@ -41,6 +45,23 @@ readArchive mainDir = do
           ++ " but found none."
         return []
 
+readTemplates :: Config -> IO (Map.Map String String)
+readTemplates config = do
+  let sDir = sourceDir config </> templates config
+  --print sDir  
+  templates <- listDirectory sDir 
+  fmap Map.fromList $ forM templates $ \template -> do
+    --print template 
+    templateContent <- readFile $ sDir </> template
+    return (template,templateContent)
+   
+
+mkGlobalContext :: Config -> IO Template.Context
+mkGlobalContext config  = do
+  css <-  readFile  $  sourceDir config   </> "styles.css"
+  return $ Map.singleton "styles.css" (Template.Template css)
+
+
 writePosts :: Config -> IO ()
 writePosts config = do
   let sDir    = sourceDir config
@@ -57,7 +78,9 @@ writeArchive config = do
       mdPosts     = sDir </> markdown config
   posts    <- readArchive mdPosts
   template <- readFile arcTemplate
-  writePage dDir template $ Post.archiveCtx posts
+  globalTemplates <- readTemplates config 
+  let allCtx = Map.union  (Post.archiveCtx posts) (Template.Template <$>  globalTemplates )
+  writePage dDir template  allCtx 
 
 writePost :: Config -> Post.Post -> IO ()
 writePost config post = do
@@ -67,7 +90,9 @@ writePost config post = do
       postTemplatePath = sDir </> templates config </> "post.html"
       postCtx          = Post.postCtx post
   postTemplate <- readFile postTemplatePath
-  writePage fullPath postTemplate postCtx
+  globalTemplates <- readTemplates config 
+  let  allCtx = Map.union  (Post.postCtx post) (Template.Template <$>  globalTemplates )
+  writePage fullPath postTemplate allCtx
 
 -- Given a destination directory, a template and a context
 -- generate an HTML page
@@ -78,26 +103,40 @@ writePage destDir template context = do
   createDirectoryIfMissing True destDir
   writeFile destFile file
 
+
+-- Delete all files file from the output directory
+-- migrate the css file to the destination directory
+--
 genHtml :: Config -> IO ()
-genHtml config = concurrently_
-  (writeArchive config)
-  (writePosts config)
+genHtml config = do
+  cleanOutputDir config
+  migrateCss config 
+  concurrently_ (writeArchive config) (writePosts config)
 
 cleanOutputDir :: Config -> IO ()
 cleanOutputDir config = do
   let dDir = destDir config
-  putStrLn dDir 
-  exists <-doesDirectoryExist dDir
+  putStrLn dDir
+  exists <- doesDirectoryExist dDir
   when exists $ do
-      files <- listDirectory dDir 
-      forM_ files $ \file -> do
-        putStrLn $ " checking " ++ file ++ "..."
-        let path  = dDir </> file 
-        isFile <- doesFileExist path 
-        if isFile
-          then do
+    files <- listDirectory dDir
+    forM_ files $ \file -> do
+      putStrLn $ " checking " ++ file ++ "..."
+      let path = dDir </> file
+      isFile <- doesFileExist path
+      if isFile
+        then do
           removeFile path
-          putStrLn  $ "file " ++ file ++ " is removed"
-          else do
+          putStrLn $ "file " ++ file ++ " is removed"
+        else do
           removeDirectoryRecursive path
-          putStrLn  $"directory " ++ file ++ " is removed"
+          putStrLn $ "directory " ++ file ++ " is removed"
+
+migrateCss :: Config -> IO ()
+migrateCss config = do
+  let cssFile = "styles.css"
+      sPath = sourceDir config </> templates config </> cssFile
+      dPath = destDir config  </> cssFile
+  print sPath
+  print dPath 
+  copyFile sPath dPath 
